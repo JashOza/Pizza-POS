434cf01efba4ac07344d889af11e6e9a311dc31b
JashOza <jashoza2108@gmail.com>
2022-10-11 14:45:23 -0400

    added sql file.

---
+++ DATABASE.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE LAB4;
+USE LAB4;
+
+CREATE TABLE pizza(
+	pizzaID INT NOT NULL auto_increment PRIMARY KEY,
+	dough VARCHAR(15),
+    crustStyle VARCHAR(15),
+    sauce VARCHAR(15),
+    delivery boolean,
+    price dec(5,2)
+    
+);
+
+create table topping(
+	toppingID INT NOT NULL auto_increment PRIMARY KEY,
+    toppingName varchar(30)
+);
+
+create table pizzaToppings(
+	pizzaToppingsID INT NOT NULL auto_increment PRIMARY KEY,
+    pizzaID int not null,
+    toppingID int not null,
+    foreign key(pizzaID) references pizza(pizzaID),
+    foreign key(toppingID) references topping(toppingID)
+);
